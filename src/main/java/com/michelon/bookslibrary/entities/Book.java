d9bb0caef578b96312f2a161d52389a1a05750c9
RaquelMichelon <raquelmichelonaulas@gmail.com>
2023-05-12 15:51:14 +0100

    orm book entity

--- src/main/java/com/michelon/bookslibrary/entities/Book.java
+++ src/main/java/com/michelon/bookslibrary/entities/Book.java
@@ -1,8 +1,8 @@
 package com.michelon.bookslibrary.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import java.util.Objects;
 
@@ -10,13 +10,21 @@ import java.util.Objects;
  * @autor raqueldarellimichelon
  * created on 12/05/23 inside the package - com.michelon.bookslibrary.entities
  **/
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "tb_book")
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+    @Column(name = "book_year")
     private Integer year;
     private String gender;
     private String platforms;
@@ -28,14 +36,14 @@ public class Book {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
         Book book = (Book) o;
-        return Objects.equals(id, book.id);
+        return id != null && Objects.equals(id, book.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return getClass().hashCode();
     }
 }
 
