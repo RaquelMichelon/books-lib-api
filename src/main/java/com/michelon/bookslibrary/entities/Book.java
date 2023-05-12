9a468d4d2a7784b3d7d883bcf8982aca7e72c958
RaquelMichelon <raquelmichelonaulas@gmail.com>
2023-05-12 15:29:04 +0100

    create Book entity

---
+++ src/main/java/com/michelon/bookslibrary/entities/Book.java
@@ -0,0 +1,22 @@
+package com.michelon.bookslibrary.entities;
+
+/**
+ * @autor raqueldarellimichelon
+ * created on 12/05/23 inside the package - com.michelon.bookslibrary.entities
+ **/
+public class Book {
+
+    private Long id;
+    private String title;
+    private Integer year;
+    private String gender;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+
+}
+
+
