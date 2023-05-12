977bf161ff8f9e8955ed7357d5b6610358742dff
RaquelMichelon <raquelmichelonaulas@gmail.com>
2023-05-12 16:29:08 +0100

    create BookDTO

---
+++ src/main/java/com/michelon/bookslibrary/BookResponseDTO.java
@@ -0,0 +1,31 @@
+package com.michelon.bookslibrary;
+
+import com.michelon.bookslibrary.entities.Book;
+import lombok.*;
+
+/**
+ * @autor raqueldarellimichelon
+ * created on 12/05/23 inside the package - com.michelon.bookslibrary
+ **/
+@Getter
+public class BookResponseDTO {
+
+    private Long id;
+    private String title;
+    private Integer year;
+    private String imgUrl;
+    private String shortDescription;
+
+    public BookResponseDTO() {
+    }
+
+    public BookResponseDTO(Book entity) {
+        id = entity.getId();
+        title = entity.getTitle();
+        year = entity.getYear();
+        imgUrl = entity.getImgUrl();
+        shortDescription = entity.getShortDescription();
+    }
+}
+
+
