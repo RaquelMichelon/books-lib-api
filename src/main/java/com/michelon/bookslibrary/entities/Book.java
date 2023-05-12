bd6399b2a7a4407d09a208e62c157fa205e22e7f
RaquelMichelon <raquelmichelonaulas@gmail.com>
2023-05-12 15:46:41 +0100

    create contructors and other methods

--- src/main/java/com/michelon/bookslibrary/entities/Book.java
+++ src/main/java/com/michelon/bookslibrary/entities/Book.java
@@ -1,9 +1,18 @@
 package com.michelon.bookslibrary.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
 /**
  * @autor raqueldarellimichelon
  * created on 12/05/23 inside the package - com.michelon.bookslibrary.entities
  **/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Book {
 
     private Long id;
@@ -16,7 +25,18 @@ public class Book {
     private String shortDescription;
     private String longDescription;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
 
 
