8032123108c69d4451e6fd8b0205d8de82f2b6ec
RaquelMichelon <raquelmichelonaulas@gmail.com>
2023-05-13 17:15:36 +0100

    create getById on service and controller

--- src/main/java/com/michelon/bookslibrary/controllers/BookController.java
+++ src/main/java/com/michelon/bookslibrary/controllers/BookController.java
@@ -1,9 +1,11 @@
 package com.michelon.bookslibrary.controllers;
 
+import com.michelon.bookslibrary.dtos.BookDTO;
 import com.michelon.bookslibrary.dtos.BookResponseDTO;
 import com.michelon.bookslibrary.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,11 @@ public class BookController {
 
     @Autowired
     private BookService bookService;
+
+    @GetMapping(value = "/{id}")
+    public BookDTO findById(@PathVariable  Long id) {
+        return bookService.findById(id);
+    }
 
     @GetMapping
     public List<BookResponseDTO> findAll() {
--- src/main/java/com/michelon/bookslibrary/services/BookService.java
+++ src/main/java/com/michelon/bookslibrary/services/BookService.java
@@ -1,9 +1,12 @@
 package com.michelon.bookslibrary.services;
 
+import com.michelon.bookslibrary.dtos.BookDTO;
 import com.michelon.bookslibrary.dtos.BookResponseDTO;
+import com.michelon.bookslibrary.entities.Book;
 import com.michelon.bookslibrary.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,6 +20,14 @@ public class BookService {
     @Autowired
     private BookRepository bookRepository;
 
+    @Transactional(readOnly = true) //ACID - Atomica Consistente Isolada e Durável
+    public BookDTO findById(Long id) {
+        //to do: create exception here
+        Book result = bookRepository.findById(id).get();
+        return new BookDTO(result);
+    }
+
+    @Transactional(readOnly = true) //announce that will not write on the db, so the transaction tends to be faster
     public List<BookResponseDTO> findAll(){
         return bookRepository.findAll().stream().map(BookResponseDTO::new).toList();
     }
